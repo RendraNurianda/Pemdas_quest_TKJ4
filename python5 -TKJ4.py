c758ae26d68d94fa7d8ccf81ef98895d1c43c323
Rendranurianda <112526315+RendraNurianda@users.noreply.github.com>
2022-09-05 08:22:59 +0700

    Add files via upload

---
+++ python5 -TKJ4.py
@@ -0,0 +1,22 @@
+# dollar ke rupiah dollar * rupiah
+
+dollar = 5
+rupiah = 14.895
+idr = dollar * rupiah
+print ("dollar dijadikan rupiah", idr)
+
+
+#uero ke yen uero * yen
+
+uero = 22
+yen = 139.27
+jpy = uero * yen
+print ("uero dijadikan yen", jpy)
+
+
+#won ke rupiah won * rupiah
+
+won = 186
+rupiah = 10.92
+idr = won * rupiah
+print ("won dijadikan rupiah", idr)
