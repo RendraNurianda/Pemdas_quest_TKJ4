25d7d2a2c1afc00a330025e55e479fea3121ae85
Rendranurianda <112526315+RendraNurianda@users.noreply.github.com>
2022-09-09 14:46:17 +0700

    Add files via upload

---
+++ UPRAK-Rendra nurianda-TKJ4.py
@@ -0,0 +1,46 @@
+# rumus volume balok panjang * lebar * tinggi
+
+panjang = 20
+lebar = 30
+tinggi =40
+
+volume_b = panjang * lebar * tinggi
+print("volume balok adalah", volume_b)
+
+
+# rumus volume kubus sisi1 * sisi2 * sisi3
+
+sisi1 = 15
+sisi2 = 35
+sisi3 = 50
+volume_k = sisi1 * sisi2 * sisi3
+print("volume kubus adalah", volume_k)
+
+
+# rumus volume limas 1/3 * luas alas (persegi) * tinggi
+
+a = 20
+b = 10
+t = 30
+volume_l = a * t / 3
+print ("volume limas adalah", volume_l)
+
+
+# rumus volume tabung 22/7 * r * r * t
+
+phi = 22/7
+r = 14
+r = 14
+t = 21
+volume_t = phi * r * r * t
+print("volume tabung adalah", volume_t)
+
+# celcius ke reamur celcius * 4/5
+
+celcius = 40
+reamur = 4/5
+suhu = 40 * 4/5
+
+print("celcius dijadikan reamur",suhu)
+
+
