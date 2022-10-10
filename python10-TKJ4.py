575687eea1c969852cdfd2df308dcc9fd599f765
Rendranurianda <112526315+RendraNurianda@users.noreply.github.com>
2022-10-10 09:45:59 +0700

    Add files via upload

---
+++ python10-TKJ4.py
@@ -0,0 +1,28 @@
+# Membuat Tipe data koleksi
+
+koleksi_data_str = ["Pisang", "Mangga", "Jambu", "Semangka"]
+
+koleksi_data_int = [200, 300, 400, 500]
+
+koleksi_data_mix = ["Rizky Billar", 100, True, "Reza Arap"]
+
+print("koleksi data string:", koleksi_data_str)
+
+print("koleksi data campuran:", koleksi_data_int)
+
+print("koleksi data campuran:", koleksi_data_mix)
+
+#buatlah 3 kumpulan data: Nama Hewan, nilai uts , nama teman
+
+
+koleksi_nama_hewan = ["Kucing", "Rusa", "Kambing"]
+
+koleksi_nilai_uts = [80, 90, 100]
+
+koleksi_nama_teman = ["Akmal", "Ade", "Anjas"]
+
+print("koleksi nama hewan:", koleksi_nama_hewan)
+
+print("koleksi nilai uts:", koleksi_nilai_uts)
+
+print("koleksi nama teman:", koleksi_nama_teman)
