053cf42a8a028702a428d4e399bfd146ff34557e
Rendranurianda <112526315+RendraNurianda@users.noreply.github.com>
2022-10-12 14:01:08 +0700

    Add files via upload

---
+++ python10.2-TKJ4.py
@@ -0,0 +1,75 @@
+# Membuat Tipe data koleksi
+
+koleksi_data_str = ["Pisang", "Mangga", "Jambu", "Semangka"]
+
+koleksi_data_int = [200, 300, 400, 500]
+
+koleksi_data_mix = ["Rizky Billar", 100, True, "Reza Arap"]
+
+print("koleksi data string:", koleksi_data_str)
+
+print("koleksi data campuran:", koleksi_data_int)
+
+print("koleksi data campuran:", koleksi_data_mix)
+
+#buatlah 3 kumpulan data: Nama Hewan, nilai uts , nama teman
+
+nama_hewan = ["Kucing", "Rusa", "Kambing", "harimau"]
+
+nilai_uts = [80, 90, 100]
+
+nama_teman = ["Akmal", "Ade", "Anjas"]
+
+print("nama hewan:", nama_hewan)
+
+print("nilai uts:", nilai_uts)
+
+print("nama teman:", nama_teman)
+
+
+# Tampilkan data koleksi dengan indeks
+
+
+# data ke 2 nama hewan
+print("data ke-2 nama hewan:", nama_hewan[1])
+
+
+# data pertama nilai uts
+print("nilai uts data pertama:", nilai_uts[0])
+
+# data terakhir nama teman
+print("data terakhir nama teman:", nama_teman[2])
+
+
+nama_hewan[2] = "babi"
+
+print(nama_hewan)
+
+nama_hewan.append("Macan")
+print(nama_hewan)
+
+#tambahkan 1 data disetiap data koleksi
+
+nama_hewan.append("Macan")
+print(nama_hewan)
+
+nilai_uts.append ("95")
+print (nilai_uts)
+
+nama_teman.append("fajri")
+print (nama_teman)
+
+
+# ubahlah data terakhir nilai uts
+
+nilai_uts[3] =  " 55 "
+print(nilai_uts)
+
+
+
+#ubahlah data ke-3 nama hewan
+
+nama_hewan[2] = " Monyet "
+print(nama_hewan)
+
+
