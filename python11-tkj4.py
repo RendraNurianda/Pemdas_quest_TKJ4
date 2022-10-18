c5a10b40a3b461c7e72eb7ff1e350dbf3980ed3f
Rendranurianda <112526315+RendraNurianda@users.noreply.github.com>
2022-10-18 09:53:08 +0700

    Add files via upload

---
+++ python11-tkj4.py
@@ -0,0 +1,29 @@
+#Membuat tuple
+
+#General
+
+gender = ("laki-laki", "perempuan")
+
+#Menggunakan fungsi tuple ()
+
+Tuple_name = tuple(["Jakarta Raya", "Depok"])
+
+#Tuple tunggal
+
+tuple_tunggal = ("Dini",)
+
+gabungan = gender + Tuple_name
+(gabungan)
+
+
+#Nama
+nama_orang = ("Novel", "Rahmadini", "Rendra", "Nurianda", "Putra")
+print (nama_orang)
+
+#Kota
+Nama_Kota = ("Jawa Timur", "Jawa Barat", "Sumatra Utara", "Jakarta Utara", "Jakarta Timur")
+print (Nama_Kota)
+
+#nama hewan
+Nama_hewan = ("Ikan Hiu", "Piranha", "Lumba-lumba", "Anjing laut", "Kuda laut")
+print (Nama_hewan)
